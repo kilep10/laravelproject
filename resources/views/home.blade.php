a19cc0e22d2af81b75069869b444a72e6bddb340
Your Name <you@example.com>
2024-10-13 21:30:21 +0500

    первое изменение

---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/home.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header">
+                <div class="logo">
+                    <img src="/img/1.jpg" alt="">
+                </div>
+                <div class="nav">
+                    <div class="nav-item">
+                        <a href="#">Главная</a>
+                    </div>
+                    <div class="nav-item">
+                        <a href="#">Массивы</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <img src="/img/1.jpg" alt="" style="width:200px">
+            <div class="riba">
+                Рыбный текст — это условный, зачастую бессмысленный текст-заполнитель, который используют вместо реального текста при разработке макетов и дизайна будущего сайта или приложения. 
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="footer">
+                <div class="text">
+                    ©Копирайт
+                </div>
+                <div class="text">
+                    Лебедев Дмитрий Константинович
+                </div>
+                <div class="text">
+                    2024.
+                </div>
+            </div>
+        </div>
+    </footer>    
+</body>
+</html>
