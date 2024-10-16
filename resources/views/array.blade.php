3750c4936eaff600d2f2bf0825c96a15244afb03
Your Name <you@example.com>
2024-10-16 20:42:36 +0500

    Задание 3

--- resources/views/array.blade.php
+++ resources/views/array.blade.php
@@ -5,14 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="\css\array.css">
+    <link rel="stylesheet" href="\css\home.css">
 </head>
 <body>
-    <div class="cards">
-        @foreach($array as $arr)
-        <p>{{$arr["id"]}}</p>
-        <p>{{$arr["title"]}}</p>
-        <p>{{$arr["price"]}}</p>
-        @endforeach
+    <div class="container">
+        <div class="cards">
+            <div class="card">
+                @foreach($array as $arr)
+                <p>{{$arr["id"]}}</p>
+                <p>{{$arr["title"]}}</p>
+                <p>{{$arr["price"]}}</p>
+                @endforeach 
+            </div>
+        </div>    
     </div>
 </body>
 </html>
