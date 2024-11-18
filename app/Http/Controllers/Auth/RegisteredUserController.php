82867a83c4488550c12bec4e05a3c1a99e2fd605
kilep10 <dima-lebedv174@mail.ru>
2024-11-18 13:10:54 +0500

    Задание8

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,16 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'tel' => ['required', 'string','max:255'],
+            'login' => ['required', 'string', 'max:255', 'unique:'.User::class],
+            'password' => ['required', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'tel' => $request->tel,
+            'login' => $request->login, 
             'password' => Hash::make($request->password),
         ]);
 
